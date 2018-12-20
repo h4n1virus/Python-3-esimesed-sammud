82faeaa638702ea659d14d8a2c3deb29f397e444
Sander Viirmaa <h4n1virus@gmail.com>
2018-12-20 03:17:09 +0200

    Add files via upload

---
+++ ylesanded/teema_6/yl04.py
@@ -0,0 +1,17 @@
+﻿# Sander Viirmaa
+# 19.12.2018
+# Ülesanne 06 - 04
+
+
+a = open('tekstifailid\\s6pru_l6ustaraamatus.txt', 'r')
+b = open('tekstifailid\\teema6_yl04.txt', 'w')
+c = a.read()
+d = c.splitlines()
+
+for x in d:
+    h = x.split()
+    b.write('{:15}{:<15}\n'.format(h[0], h[1]))
+
+a.close()
+b.close()
+
