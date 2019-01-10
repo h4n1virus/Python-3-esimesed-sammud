6af37571a41779e10f9db726a57ca68b033073fc
Sander Viirmaa <h4n1virus@gmail.com>
2019-01-10 14:57:48 +0200

    Lihtsustasin koodi

--- ylesanded/teema_7/yl03.py
+++ ylesanded/teema_7/yl03.py
@@ -5,23 +5,6 @@
 from os import system
 from math import pi
 
-def kuupV(a):
-    v = a**3
-    return v
-
-def keraV(r):
-    v = 4 / 3 * pi * r**3
-    return v
-
-def koonuseV(sp, st):
-    v = 1 / 3 * sp * st
-    return v
-
-def silinderV(h, r):
-    v = pi * r**2 * h
-    return v
-
-
 while True:
     system('cls')
     print("********** LEIAME RUUMALA **********")
@@ -31,29 +14,32 @@ while True:
     print("3 Koonus")
     print("4 Silinder")
     print("5 Tagasi\n")
-
     a = int(input("Sisesta soovitud kujundi number: "))
+
     if a == 1:
         a = float(input("Valisid kuubi. Sisesta kuubi külg: "))
-        print('\nKuubi ruumala on: {}'.format(kuupV(a)))
+        v = a**3
+        print('\nKuubi ruumala on: {}'.format(v))
         input()
         continue
     elif a == 2:
         r = float(input("Valisid kera. Sisesta kera raadius: "))
-        print('\nKera ruumala on: {}'.format(keraV(r)))
+        v = 4 / 3 * pi * r**3
+        print('\nKera ruumala on: {}'.format(v))
         input()
         continue
     elif a == 3:
         sp = float(input("Valisid koonuse. Sisesta koonuse kõrgus: "))
-        st = float(
-            input("Valisid koonuse. Sisesta koonuse põhjapindala: "))
-        print("Koonuse ruumala on: {}".format(koonuseV(sp, st)))
+        st = float(input("Valisid koonuse. Sisesta koonuse põhjapindala: "))
+        v = 1 / 3 * sp * st
+        print("Koonuse ruumala on: {}".format(v))
         input()
         continue
     elif a == 4:
         h = float(input("Valisid Silindri. Sisesta silindri kõrgus: "))
         r = float(input("Valisid Silindri. Sisesta silindri raadius: "))
-        print("Silindri ruumala on: {}".format(silinderV(h, r)))
+        v = pi * r**2 * h
+        print("Silindri ruumala on: {}".format(v))
         input()
         continue
     elif a == 5:
