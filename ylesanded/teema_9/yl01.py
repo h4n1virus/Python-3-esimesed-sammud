07181c061927cfba828ec4059d48274b7a60f93f
Chisa Yomoda <h4n1virus@gmail.com>
2022-05-19 23:19:01 +0300

    Update yl01.py

--- ylesanded/teema_9/yl01.py
+++ ylesanded/teema_9/yl01.py
@@ -4,33 +4,33 @@
 
 from datetime import datetime
 
-date = datetime.today()
 
-def switcher(a):
-    if date.month == 1:
+def switcher(month):
+    if month == 1:
         return "Jaanuar"
-    elif date.month == 2:
+    elif month == 2:
         return "Veebruar"
-    elif date.month == 3:
+    elif month == 3:
         return "Märts"
-    elif date.month == 4:
+    elif month == 4:
         return "Aprill"
-    elif date.month == 5:
+    elif month == 5:
         return "Mai"
-    elif date.month == 6:
+    elif month == 6:
         return "Juuni"
-    elif date.month == 7:
+    elif month == 7:
         return "Juuli"
-    elif date.month == 8:
+    elif month == 8:
         return "August"
-    elif date.month == 9:
+    elif month == 9:
         return "September"
-    elif date.month == 10:
+    elif month == 10:
         return "Oktoober"
-    elif date.month == 11:
+    elif month == 11:
         return "November"
-    elif date.month == 12:
+    elif month == 12:
         return "Detsember"
 
+date = datetime.today()
 print('{}. {} {}'.format(date.day, date.strftime("%B"), date.year))
 print('{}. {}. {}'.format(date.day, switcher(date.month), date.year))
